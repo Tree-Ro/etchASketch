80f87d4ac12642a7e7cf9ce18bf6d10fe43a2676
David Steiner <david1.steiner@gmail.com>
2023-11-16 12:54:31 +0100

    Added & Created createGrid() {createRows() createCells()}, creates 16x16

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const container = document.querySelector('#mainContainer');
 
+function createRows() {
+    for (i = 0; i < 16; ++i) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        container.appendChild(row);
+    }
+}
+
+function createCells() {
+    const rows = document.querySelectorAll('#mainContainer .row');
+    rows.forEach((row) => {
+        for (i = 0; i < rows.length; ++i) {
+            const cell = document.createElement('div');
+            cell.setAttribute('class', 'cell');
+            row.appendChild(cell);
+        }
+    });
+}
+
+function createGrid() {
+    createRows();
+    createCells();
+}
